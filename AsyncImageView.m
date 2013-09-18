d751275a5f42f03fd2f0ef8a221783ff3168e90d
Ronald Klip <ronald.klip@contentecontent.com>
2013-09-18 12:29:53 +0200

    enabled ARC

--- AsyncImageView.m
+++ AsyncImageView.m
@@ -19,15 +19,10 @@
 
 - (void)dealloc {
 	[connection cancel]; //in case the URL is still downloading
-	[connection release];
-	[data release]; 
-    [super dealloc];
 }
 
 
 - (void)loadImageFromURL:(NSURL*)url {
-	if (connection!=nil) { [connection release]; } //in case we are downloading a 2nd image
-	if (data!=nil) { [data release]; }
 	
 	NSURLRequest* request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0];
 	connection = [[NSURLConnection alloc] initWithRequest:request delegate:self]; //notice how delegate set to self object
@@ -44,7 +39,6 @@
 //the URL connection calls this once all the data has downloaded
 - (void)connectionDidFinishLoading:(NSURLConnection*)theConnection {
 	//so self data now has the complete image 
-	[connection release];
 	connection=nil;
 	if ([[self subviews] count]>0) {
 		//then this must be another image, the old one is still in subviews
@@ -52,7 +46,7 @@
 	}
 	
 	//make an image view for the image
-	UIImageView* imageView = [[[UIImageView alloc] initWithImage:[UIImage imageWithData:data]] autorelease];
+	UIImageView* imageView = [[UIImageView alloc] initWithImage:[UIImage imageWithData:data]];
 	//make sizing choices based on your needs, experiment with these. maybe not all the calls below are needed.
 	imageView.contentMode = UIViewContentModeScaleAspectFit;
 	imageView.autoresizingMask = ( UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight );
@@ -61,7 +55,6 @@
 	[imageView setNeedsLayout];
 	[self setNeedsLayout];
 	
-	[data release]; //don't need this any more, its in the UIImageView now
 	data=nil;
 }
 
@@ -74,7 +67,7 @@
 	}
 	
 	//make an image view for the image
-	UIImageView* imageView = [[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"icon.png"]] autorelease];
+	UIImageView* imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"icon.png"]];
 	//make sizing choices based on your needs, experiment with these. maybe not all the calls below are needed.
 	imageView.contentMode = UIViewContentModeScaleAspectFit;
 	imageView.autoresizingMask = ( UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight );
--- MapKit.m
+++ MapKit.m
@@ -106,7 +106,6 @@
 		annotation.selected = selected;
 		
 		[self.mapView addAnnotation:annotation];
-		[annotation release];
         [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
 	}
 }
@@ -226,14 +225,13 @@
 	else
 		annView.pinColor = MKPinAnnotationColorRed;
 
-	AsyncImageView* asyncImage = [[[AsyncImageView alloc] initWithFrame:CGRectMake(0,0, 50, 32)] autorelease];
+	AsyncImageView* asyncImage = [[AsyncImageView alloc] initWithFrame:CGRectMake(0,0, 50, 32)];
 	asyncImage.tag = 999;
 	if (phAnnotation.imageURL)
 	{
 		NSURL *url = [[NSURL alloc] initWithString:phAnnotation.imageURL];
 		[asyncImage loadImageFromURL:url];
-		[ url release ];
-	} 
+	}
 	else 
 	{
 		[asyncImage loadDefaultImage];
@@ -295,7 +293,6 @@
         self.childView = nil;
 	}
     self.buttonCallback = nil;
-    [super dealloc];
 }
 
 @end
